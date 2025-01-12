099ba8592cbd7c5fb7d52c16c7c5490c940015ad
Rekuiem84 <morinijoshua@gmail.com>
2025-01-12 02:31:09 +0100

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: "https://rekuiem84.github.io",
+	base: "/pokedex",
+});
